15cf90acdaa5b83fa977add2ba628b6b60af5f0f
Reed Zitting <reedgzitting@gmail.com>
2024-05-08 22:08:32 -0600

    Cards package is made

--- deckOfDeath/src/cards/Deck.java
+++ deckOfDeath/src/cards/Deck.java
@@ -1,4 +1,23 @@
 package cards;
 
+import java.util.HashSet;
+
 public class Deck {
+
+    public HashSet<Card> deck;
+
+    public Deck(){
+        reset();
+    }
+
+    public void reset(){
+        for (Suit suit : Suit.values()){
+            for (CardID cardID : CardID.values()){
+                deck.add(new Card(suit,cardID));
+            }
+        }
+        deck.add(new Card(Suit.JOKER, null));
+        deck.add(new Card(Suit.JOKER, null));
+    }
+
 }
--- deckOfDeath/src/cards/Suit.java
+++ deckOfDeath/src/cards/Suit.java
@@ -4,5 +4,6 @@ public enum Suit {
     SPADES,
     HEARTS,
     DIAMONDS,
-    CLUBS
+    CLUBS,
+    JOKER
 }
